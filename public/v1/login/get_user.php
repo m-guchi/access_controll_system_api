7157461143f22d80aa77e675645d5d90a7e7ba22
guchimina -thinkpad <kazu9sum.mi@gmail.com>
2021-11-25 22:58:34 +0900

    不要ファイル削除

--- public/v1/login/get_user.php
+++ public/v1/login/get_user.php
@@ -12,18 +12,11 @@ if(!$cert->is_continue()){
 
 $db = new DB();
 try{
-    $db->pdo->beginTransaction();
     $sql = "SELECT login_user_id,login_id,login_user_name,auth_group FROM login_users WHERE login_user_id=:login_user_id";
     $sth_user = $db->pdo->prepare($sql);
     $sth_user->bindValue(":login_user_id",$cert->login_user_id);
     $sth_user->execute();
-    $sql = "SELECT gate_id FROM login_range_gate WHERE login_user_id = :login_user_id ORDER BY gate_id ASC";
-    $sth_gate = $db->pdo->prepare($sql);
-    $sth_gate->bindValue(":login_user_id",$cert->login_user_id);
-    $sth_gate->execute();
-    $db->pdo->commit();
 }catch(PDOException $e){
-    $db->pdo->rollBack();
     $this->code = 500;
     return $cert->return->set_db_error($e);
 }
