56f536a9a819ed39ffe40e497cfefb48dd60fa3c
minamiguchi <kazu9sum.mi@gmail.com>
2023-11-15 17:24:00 +0900

    change DB data

--- config/db.php
+++ config/db.php
@@ -2,29 +2,34 @@
 
 namespace DB;
 
-include(__DIR__ . "/env/db_mysql.php");
+// include(__DIR__ . "/env/db_mysql.php");
 
 use PDO;
 use PDOException;
+use \Symfony\Component\Yaml\Yaml;
 
 class DB
 {
     public $pdo;
+    private $env;
 
     public function __construct()
     {
+        $input = file_get_contents(__DIR__."/../../../.env/db_key.yaml");
+        $this->env = Yaml::parse($input);
         $this->pdo = $this->pdo();
     }
 
     public function pdo()
     {
-        global $env_mysql;
         try{
-            $dns = "mysql:dbname=".$env_mysql['dbname'].";host=".$env_mysql['host'].";charset=utf8mb4";
-            $pdo = new PDO($dns,$env_mysql["user"],$env_mysql["password"]);
-            // $pdo = new PDO('sqlite:db/e_mgmt.db');
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            $driver_option = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ];
+            $dns = "mysql:dbname=app_access_controll;host={$this->env["host"]};charset=utf8mb4";
+            $pdo = new PDO($dns ,$this->env["user"],$this->env["pass"],$driver_option);
         }catch(PDOException $error){
             header("Content-Type: application/json; charset=utf-8", true, 500);
             echo json_encode(["ok"=>false,"token"=>"","error" => ["type" => "db_error","message"=>$error->getMessage()]]);
--- config/env/db_mysql.php
+++
@@ -1,7 +0,0 @@
-<?php
-$env_mysql = [
-    "dbname" => "e_mgmt",
-    "host" => "localhost",
-    "user" => "e_mgmt_user",
-    "password" => "DYVO0rCEcIkyX8kmrfzgvmOg"
-];
